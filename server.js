605ea89621a11e034a8c3c8a33b1aaba71487220
sonam <sonamgadre084@gmail.com>
2025-01-18 15:43:51 +0530

    changing a port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({
         success: true,
-        message: "Hello from the node-app server"
+        message: "Hello from the node-app-testing server"
     })
 })
 
